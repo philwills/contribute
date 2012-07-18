a8da89da82b52a8fced4d03155d064ff202ba027
Maxwell Harlow <max.harlow@guardian.co.uk>
2012-07-18 14:31:34 +0100

    Endpoints for responses

--- contribute-api/src/main/scala/com.gu.contribute.api/Responses.scala
+++ contribute-api/src/main/scala/com.gu.contribute.api/Responses.scala
@@ -6,8 +6,10 @@ case class JournalistResponse(status: String, content: Journalist)
 
 case class ContributorResponse(status: String, content: Contributor)
 
-case class ContributorRequestResponse(status: String, content: List[Request])
+case class ContributorRequestsResponse(status: String, content: List[Request])
 
 case class RequestResponse(status: String, content: Request)
+
+case class RequestResponsesResponse(status: String, content: List[Response])
 
 case class ResponseResponse(status: String, content: Response)
