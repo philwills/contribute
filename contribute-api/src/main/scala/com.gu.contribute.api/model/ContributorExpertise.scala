aaea2abb8f58fa31ef7a2efae7200dafbc80431c
Maxwell Harlow <max.harlow@guardian.co.uk>
2012-07-18 20:36:20 +0100

    Removed unnecessary imports

--- contribute-api/src/main/scala/com.gu.contribute.api/model/ContributorExpertise.scala
+++ contribute-api/src/main/scala/com.gu.contribute.api/model/ContributorExpertise.scala
@@ -1,16 +1,6 @@
 package com.gu.contribute.api.model
 
-import com.novus.salat.annotations._
-import org.bson.types.ObjectId
-import com.novus.salat.dao.SalatDAO
-import com.gu.contribute.api.mongo.MongoDataSource
-import com.gu.contribute.api.CasbahConverstionHelpers
-import com.novus.salat._
-import com.novus.salat.global._
-import com.mongodb.casbah.Imports._
-import com.gu.contribute.api.SalatTypeConversions._
 import com.gu.management.Loggable
-import com.mongodb.casbah.commons.MongoDBObject
 import org.joda.time.DateTime
 
 case class ContributorExpertise(what: String,
