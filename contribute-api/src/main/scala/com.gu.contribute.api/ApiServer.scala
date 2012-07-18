8fcbe0df0d5eaf4a3680238cc8199c0b46d973ce
Philip Wills <philip.wills@guardian.co.uk>
2012-07-18 15:19:18 +0100

    Working locally, but not in uber-jar yet

--- contribute-api/src/main/scala/com.gu.contribute.api/ApiServer.scala
+++ contribute-api/src/main/scala/com.gu.contribute.api/ApiServer.scala
@@ -2,13 +2,15 @@ package com.gu.contribute.api
 
 import org.eclipse.jetty.server.Server
 import org.eclipse.jetty.webapp.WebAppContext
+import org.eclipse.jetty.servlet.ServletContextHandler
+import java.io.File
 
 object ApiServer extends App {
   val server = new Server(8080)
 
   val context = new WebAppContext()
   context.setContextPath("/")
-  context.setResourceBase("./contribute-api/src/main/webapp")
+  context.setResourceBase("./src/main/webapp")
   context.setParentLoaderPriority(true)
 
   server.setHandler(context)
--- project/plugins/build.sbt
+++
@@ -1,9 +0,0 @@
-resolvers ++= Seq(
-  "Web plugin repo" at "http://siasia.github.com/maven2",
-  "Guardian GitHub Release" at "http://guardian.github.com/maven/repo-releases",
-  Resolver.url("Typesafe repository", new java.net.URL("http://typesafe.artifactoryonline.com/typesafe/ivy-releases/"))(Resolver.defaultIvyPatterns)
-)
-
-//libraryDependencies <+= sbtVersion(v => "com.github.siasia" %% "xsbt-web-plugin" % (v+"-0.2.7"))
-
-scalaVersion := "2.9.1"
