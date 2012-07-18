53ab5a5a48e5c7f71aee9e546fcda352bd5dda5b
Philip Wills <philip.wills@guardian.co.uk>
2012-07-18 12:06:02 +0100

    Added update endpoint, which doesn't do much
    Also lots of JS futzing

--- sieve/app/controllers/Api.scala
+++ sieve/app/controllers/Api.scala
@@ -3,11 +3,20 @@ package controllers
 import net.liftweb.json.{NoTypeHints, Serialization}
 import play.api.mvc._
 import play.api.mvc.Results._
+import play.api.{Logger, Play}
+import play.api.libs.json.JsValue
 
 object Api {
   implicit val formats = Serialization.formats(NoTypeHints)
 
   def submissions = Action {
     Ok(Serialization.write(Scaffolding.submissions))
+  }
+
+  def updateSubmission = Action { req =>
+    Ok(req.body.asJson.map { js: JsValue =>
+      Logger.info(js.toString())
+      js.toString()
+    }.getOrElse{Logger.error(req.body.toString());req.body.toString()})
   }
 }
